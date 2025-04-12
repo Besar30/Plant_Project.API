2a8ad74e8ffb5969d3ed097cadc4f4af7f00e2a3
AhmedZidan399 <ahmed.zidan.new10@gmail.com>
2025-04-12 23:29:53 +0200

    test

--- Plant_Project.API/Program.cs
+++ Plant_Project.API/Program.cs
@@ -23,6 +23,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseStaticFiles();
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseAuthentication();
